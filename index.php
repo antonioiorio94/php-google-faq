a73e51e2994fd12e6137d4c1dae4d5547147ada6
Antonio Iorio <antonioiorio94@gmail.com>
2022-05-04 16:15:48 +0200

    init

---
+++ index.php
@@ -0,0 +1,13 @@
+<!--
+  Riscrivere questa pagina del sito google
+https://policies.google.com/faq.
+Ci sono diverse domande con relative risposte.
+Gestire il “Database” e la visualizzazione di queste domande e risposte con PHP.
+Mi raccomando di gestire per prima cosa la visualizzazione dei dati, anche in maniera grezza, e solo alla fine rendere più accattivante la pagina.
+-->
+
+<?php   
+
+phpinfo();
+
+?>
